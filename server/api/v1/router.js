2cd3bc4b7198e10198c19645b1e28c60104a9a55
Kobi Ezra <kobiye1@gmail.com>
2022-09-21 17:15:35 +0300

    Prepare for deployment (#3)
    * prepare for deployment

--- server/api/v1/router.js
+++ server/api/v1/router.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const path = require('path')
+
 const FreefitController = require('./controller');
 
 const router = express.Router();
 
 router.post('/freefit-search', FreefitController.validateSearch, FreefitController.searchInFreeFit)
 router.get('/freefit-search', FreefitController.freeFitDataArray)
+
+
+router.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, '../../../build', "index.html"));
+});
 
 module.exports = router;
