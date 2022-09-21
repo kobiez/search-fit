cc577b8cf855ab5255333eeac0f5dbdd2ed3d4d2
Kobi Ezra <kobiye1@gmail.com>
2022-09-21 18:39:32 +0300

    -

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 const app = require('./app');
-const port = process.env.PORT || 5000
+const port = process.env.PORT || process.env.SERVER_PORT
 
 app.listen(port, () => {
     console.log(`server is up at port ${port}`)
